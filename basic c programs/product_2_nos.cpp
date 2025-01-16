41cc31d3f4a8f50cf07edc15d51979b9fc1b9760
Abir Shaikh <shaikhabir044@gmail.com>
2025-01-16 12:35:14 +0530

    Add files via upload

---
+++ basic c programs/product_2_nos.cpp
@@ -0,0 +1,12 @@
+//Write a C program to multiply two numbers (4 and 5) and display its product.
+
+#include <stdio.h>
+
+int main()
+{
+	int a=4,b=5,res;
+	printf("a is %d and b is %d\n",a,b);
+	
+	res= a*b;
+	printf("a * b is %d\n",res);
+}
