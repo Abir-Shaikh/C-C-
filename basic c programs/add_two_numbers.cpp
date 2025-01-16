02749a7badedad1a654538c379b318177fbae616
Abir Shaikh <shaikhabir044@gmail.com>
2025-01-16 12:34:56 +0530

    Add files via upload

---
+++ basic c programs/add_two_numbers.cpp
@@ -0,0 +1,14 @@
+//Write a C program to add two numbers (2 and 6) and display its sum.
+
+
+#include <stdio.h>
+
+int main()
+{
+	int a=2,b=6,res;
+	
+	printf("a is %d and b is %d\n",a, b);
+	
+	res= a+b;
+	printf("a + b is %d\n",res);
+}
