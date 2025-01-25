5d933c603a0a09e60699893cb5af57b87a0c0b67
Abir Shaikh <shaikhabir044@gmail.com>
2025-01-25 20:27:48 +0530

    Add files via upload

---
+++ basic c programs/Ascii.cpp
@@ -0,0 +1,9 @@
+//Write a program to display the ASCII value of a character entered by the user.
+
+#include<stdio.h>
+
+int main(){
+	char a = 'c';
+	
+	printf("the ascii value of %a is %d",a,a);	
+}
